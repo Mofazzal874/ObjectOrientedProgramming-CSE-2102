f55b339333ad8f73b7c28e2f104d7978adc69df4
Mofazzal874 <mofazzalhossen874@gmail.com>
2023-07-13 01:10:23 +0600

    restrict throw in function

--- p2019_8_d.cpp
+++ p2019_8_d.cpp
@@ -3,14 +3,17 @@ using namespace std;
 typedef long long ll;
 
 int f(int x) throw(int , double){  //here we are restricting the function to defaulty throw only for int and double 
-                                    //throw 1 and 0 will not be thrown
+                                    //only throw for int and double will be thrown to the main .Other throw will not be thrown 
+                                    
     if(x == 1) throw 1; 
-    else throw 0 ; 
+    else throw "p" ; 
     return 0 ; 
 }
 int main(){
+    
     try{
-        f(1) ; 
+        f(5) ;
+        
     }
     catch(int){
         cout<<"Exception type integer is detected\n" ; 
